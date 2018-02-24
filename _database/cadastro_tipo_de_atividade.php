63b4f152938faca0710e3182f3316f3cfd0a041d
root <root@mj>
2018-02-24 18:19:26 -0300

    Cadastro de tipo de atividade

---
+++ _database/cadastro_tipo_de_atividade.php
@@ -0,0 +1,25 @@
+<?php
+
+
+    require_once('db.class.php');
+
+
+    $tipo = $_POST["input-tipo-tipo_de_atividade"];
+    $carga_hr_max = $_POST["input-carga_hr_max-tipo_de_atividade"];
+    $carga_hr_min = $_POST["input-carga_hr_min-tipo_de_atividade"];
+   
+    $objDb = new db();
+
+    $link = $objDb->conecta_mysql();
+
+    $sql = "INSERT INTO tipo_atividade(id, tipo, carga_hr_max, carga_hr_min) VALUES (NULL,'$tipo','$carga_hr_max', '$carga_hr_min')";
+
+
+
+    //executar a query
+    if (mysqli_query($link, $sql)) {
+        echo "Tipo de atividade registrado com sucesso!";
+    } else {
+        echo "Erro ao registrar tipo de atividade!";
+    }
+?>
