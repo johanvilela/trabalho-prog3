cd597f08adccc724135bc53741426f26461d3334
root <root@mj>
2018-02-27 21:30:44 -0300

    Modificando login

---
+++ _database/cadastro_coordenador.php
@@ -0,0 +1,27 @@
+<?php
+
+
+    require_once('db.class.php');
+
+
+    // $nome = $_POST["input-coord-nome"];
+    $email = $_POST["input-coord-email"];
+    $senha = $_POST["input-coord-senha"];
+   
+    $objDb = new db();
+
+    $link = $objDb->conecta_mysql();
+
+    $sql = "INSERT INTO coordenador(id,email,senha) VALUES (NULL,'$email','$senha')";
+
+
+
+    //executar a query
+    if (mysqli_query($link, $sql)) {
+        echo "Coordenador registrado com sucesso!";
+    } else {
+        echo "Erro ao registrar coordenador!";
+    }
+
+    $objDb->desconecta_mysql();
+?>
